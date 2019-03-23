d076ef4633da88f802f8e3f74e5716541584f7f7
Eric M <eric.mu@uleth.ca>
2019-03-22 22:38:03 -0600

    Added recursive traversal

--- fightdata.py
+++ fightdata.py
@@ -44,7 +44,6 @@ process = CrawlerProcess({
     'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
     'FEED_FORMAT': 'csv',
     'FEED_URI': 'data.csv'
-
 })
 
 URL = 'https://www.sherdog.com/fighter/Khabib-Nurmagomedov-56035'
